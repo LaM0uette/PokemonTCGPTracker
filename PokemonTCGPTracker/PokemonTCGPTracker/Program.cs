b8c6163d51713f4e45429608ec103217c79fe7bf
LaM0uette <lam0uette.dev@gmail.com>
2025-11-02 10:02:05 +0100

    setup

--- PokemonTCGPTracker/PokemonTCGPTracker/Program.cs
+++ PokemonTCGPTracker/PokemonTCGPTracker/Program.cs
@@ -1,12 +1,21 @@
+using Microsoft.AspNetCore.HttpOverrides;
 using PokemonTCGPTracker.Components;
 
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+    options.KnownNetworks.Clear();
+    options.KnownProxies.Clear();
+});
+
 builder.Services.AddRazorComponents()
     .AddInteractiveWebAssemblyComponents();
 
-var app = builder.Build();
+builder.Services.AddSignalR();
+
+WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -16,7 +25,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
